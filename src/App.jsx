fc3b5f61b2d6b33360653165b45d08fe5db7ea1a
Kudese <d.kuprichenkov@gmail.com>
2022-12-22 00:39:36 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import Portfolio from './components/Portfolio/portfolio';
+;
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <Portfolio user/>
     </div>
   );
 };
---
+++ src/components/Portfolio/portfolio.jsx
@@ -0,0 +1,7 @@
+import s from "../Portfolio/Portfolio.module.css";
+console.log(s)
+export default function Portfolio ({text}){
+console.log(text)
+
+
+}
