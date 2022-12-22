618b16829bc21f407241b122a1e6c70448ea58e9
Kudese <d.kuprichenkov@gmail.com>
2022-12-22 09:38:33 +0200

    2 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import React from 'react';
 import Portfolio from './components/Portfolio/portfolio';
-;
+import user from 'components/Portfolio/user';
+
+import Statistics from "./components/Statistics/Statistics";
+import data from './components/Statistics/data'
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div>
       React homework template
-      <Portfolio user/>
+      <Portfolio
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      
     </div>
   );
 };
--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,7 +1,48 @@
-import s from "../Portfolio/Portfolio.module.css";
-console.log(s)
-export default function Portfolio ({text}){
-console.log(text)
+import PropTypes from 'prop-types';
+import s from '../Portfolio/Portfolio.module.css';
 
+export default function Portfolio({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt={username} className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
 
+      <ul className={s.stats}>
+        <li className={s.iteam}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li className={s.iteam}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li className={s.iteam}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+Portfolio.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  localStorage: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
