6a28a62c62fb7022760326b8adc5f89ed67bb01f
Kudese <d.kuprichenkov@gmail.com>
2023-01-02 06:08:07 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ import s from '../FriendList/FriendList.module.css';
 import PropTypes from 'prop-types'
 import FrendCards from './FrendCards';
 export default function FriendList({ friends }) {
-  //   console.log(friends);
+
   return (
     <ul className={s.friend_list}>
       {friends.map(el => FrendCards(s,el)
